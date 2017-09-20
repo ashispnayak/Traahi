81c1845fafe35102e1cd33accb268b8eba488b19
Ashis Nayak <ashispnayak@gmail.com>
2017-09-20 17:06:07 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/invincix/khanam/Welcome_activity.java
@@ -0,0 +1,44 @@
+package com.invincix.khanam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+
+public class Welcome_activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_activity);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intentc = new Intent(Welcome_activity.this,MainActivity.class);
+                    startActivity(intentc);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        finish();
+    }
+
+}
+
+
+
